7c739aee23150d13309d919ad22feb6fa6e1e8d8
Ievgen Biriukov <>
2021-05-04 16:57:50 +0000

    Added two functions: randomNumberGenerator() and loadGrid()

--- script.js
+++ script.js
@@ -29,3 +29,56 @@
 	  setTimeout(function(){ document.getElementById("exitAppBox").style.display = 'none'; }, 3000);
 	  
 }
+
+//********Random Number Generator Section*************
+	function randomNumberGenerator(){
+		
+		var randNumList = new Array();
+		document.getElementById("testField").innerHTML = "test";
+      
+		var n = 15;
+		  
+		do {
+			// Generating random number
+			const randomNumber = randomN();
+		  
+			// Pushing into the array only 
+			// if the array does not contain it
+			if (!randNumList.includes(randomNumber)) {
+				randNumList.push(randomNumber);
+			}
+		 
+		} while (randNumList.length < n);
+  	  
+	 
+	    function randomN(){
+	      var x = Math.floor((Math.random() * 15) + 1);
+		  return x;
+	    }
+	  
+	  return randNumList;
+	}
+	
+	//****************Load Grid Section*************
+	function loadGrid(){
+	  
+	 var randN = randomNumberGenerator(); 
+	  
+	 var gridArray = new Array(4);
+	 for (var i = 0; i < gridArray.length; i++) {
+			gridArray[i] = new Array(3);
+		}
+	 var rows = 4;
+	 var cels = 4;
+	 var n = 0; 
+	  
+	 document.getElementById("testField2").innerHTML = randN.toString();
+	  				
+        for (var b =0; b < rows; b++){	
+	      for (var j =0; j < cels; j++){
+	       gridArray[b][j] = randN[n];
+		   n++;
+		}
+	  }     
+       return gridArray;
+	}
